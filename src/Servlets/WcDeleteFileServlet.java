7e38c657a504df150808490c2efa86a1f68f1540
amira <amiraloni7@gmail.com>
2019-10-28 14:32:34 +0200

    push with hope

---
+++ src/Servlets/WcDeleteFileServlet.java
@@ -0,0 +1,35 @@
+package Servlets;
+
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "WcDeleteFileServlet", urlPatterns = {"/delete_file"})
+public class WcDeleteFileServlet extends HttpServlet {
+
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        Map<String,Object> map=new HashMap<String,Object>();
+        String filePath = req.getParameter("path");
+        System.out.println(filePath);
+        File file = new File(filePath);
+        file.delete();
+        map.put("result",true);
+        write(res, map);
+    }
+
+    private void write(HttpServletResponse res, Map<String, Object> map) throws IOException {
+        res.setContentType("application/json");
+        res.setCharacterEncoding("UTF-8");
+        res.getWriter().write(new Gson().toJson(map));
+    }
+}
